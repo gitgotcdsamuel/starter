30c08066ad2626ba1594937844b4dd13311bd65e
cdSamuel <122785996+gitgotcdsamuel@users.noreply.github.com>
2023-03-08 18:37:59 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="hey, you are here!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <vscode>pages/index.js</code>
         </p>
       </main>
 
